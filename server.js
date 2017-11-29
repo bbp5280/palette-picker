d4e84f8d8bbbad2fa337f9fd58f3d39d73594ffd
Ben Porter <bbp5280@gmail.com>
2017-11-28 17:09:13 -0700

    initial commit

---
+++ server.js
@@ -0,0 +1,10 @@
+const express = require('express');
+const app = express();
+const path = require('path');
+
+app.set('port', process.env.PORT || 3000);
+app.use(express.static(path.join(__dirname, 'public')));
+
+app.listen(app.get('port'), () => {
+  console.log(`${app.locals.title} is running on ${app.get('port')}.`);
+});
