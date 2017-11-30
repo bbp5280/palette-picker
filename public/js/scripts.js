ab8f445c84822a4f8fb60d51c4d3bbbfd7bf0421
Ben Porter <bbp5280@gmail.com>
2017-11-29 21:51:55 -0700

    Add lock images and toggle locked and unlocked

--- public/js/scripts.js
+++ public/js/scripts.js
@@ -10,6 +10,19 @@ const createColorPalette = () => {
   }
 };
 
+const lockColors = (e) => {
+    const src = $(e.target)
+
+    if(src.attr('src') === './assets/unlocked.png') {
+        src.attr('src', './assets/locked.png')
+        src.closest('.palettes').toggleClass('locked')
+    } else {
+        src.attr('src', './assets/unlocked.png')
+        src.closest('.palettes').toggleClass('locked')
+    }
+};
+
 
 $(document).ready(createColorPalette);
 $('.new-palette').on('click', createColorPalette);
+$('.palette-container').on('click', '.lock-img', (e) => lockColors(e));
