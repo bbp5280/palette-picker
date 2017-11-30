0e9791b4b7edab9bd2b8dfca10d530c3570bbf97
Ben Porter <bbp5280@gmail.com>
2017-11-30 14:04:11 -0700

    Add projects get fetch, and appened them to the dom

--- public/js/scripts.js
+++ public/js/scripts.js
@@ -4,27 +4,52 @@ const generateColor = () => {
 
 const createColorPalette = () => {
   for (let i = 1; i < 6; i++){
-    if(!$(`.color-${i}`).hasClass('locked')){
-    const hexColor = generateColor();
-    $(`.color-${i}`).css('background-color', hexColor);
-    $(`.color-${i}-text`).text(hexColor);
-  }
- }
-};
-
-const lockColors = (e) => {
-    const target = $(e.target)
-
-    if(target.attr('src') === './assets/unlocked.png') {
-        target.attr('src', './assets/locked.png')
-        target.closest('.palettes').toggleClass('locked')
-    } else {
-        target.attr('src', './assets/unlocked.png')
-        target.closest('.palettes').toggleClass('locked')
+    if (!$(`.color-${i}`).hasClass('locked')){
+      const hexColor = generateColor();
+      $(`.color-${i}`).css('background-color', hexColor);
+      $(`.color-${i}-text`).text(hexColor);
     }
+  }
+};
+
+const lockColors = (event) => {
+  const target = $(event.target);
+
+  if (target.attr('src') === './assets/unlocked.png') {
+    target.attr('src', './assets/locked.png');
+    target.closest('.palettes').toggleClass('locked');
+  } else {
+    target.attr('src', './assets/unlocked.png');
+    target.closest('.palettes').toggleClass('locked');
+  }
+};
+
+const getProjects = () => {
+  return fetch('/api/v1/projects')
+    .then(response => response.json())
+    .then(projects => {
+      buildProjects(projects);
+    console.log(projects);
+    })
+    .catch(error => console.log(error));
+};
+
+const buildProjects = (projects) => {
+  return projects.forEach(project => {
+    return $('.projects-container').append(`<div class='project-details'>
+      <h2>${project.project_name}</h2>
+    </div>`);
+  });
+};
+
+const getProjectPalettes = () => {
+
 };
 
 
-$(document).ready(createColorPalette);
+$(document).ready(() => {
+  createColorPalette();
+  getProjects();
+});
 $('.new-palette').on('click', createColorPalette);
-$('.palette-container').on('click', '.lock-img', (e) => lockColors(e));
+$('.palette-container').on('click', '.lock-img', (event) => lockColors(event));
--- server.js
+++ server.js
@@ -6,78 +6,69 @@ const path = require('path');
 app.set('port', process.env.PORT || 3000);
 app.use(express.static(path.join(__dirname, 'public')));
 app.use(bodyParser.json());
-app.use(bodyParser.urlencoded({ extended: true }));
+app.use(bodyParser.urlencoded({extended: true}));
 
 const environment = process.env.NODE_ENV || 'development';
 const configuration = require('./knexfile')[environment];
 const database = require('knex')(configuration);
 
 app.get('/api/v1/projects', (request, response) => {
-  database('projects').select()
-    .then(projects => {
-      return response.status(200).json(projects);
-    })
-    .catch(error => {
-      return response.status(500).json(error);
-    });
+  database('projects').select().then(projects => {
+    return response.status(200).json(projects);
+  }).catch(error => {
+    return response.status(500).json(error);
+  });
 });
 
 app.get('/api/v1/projects/:id/palettes', (request, response) => {
-  database('palettes').where('project_id', request.params.id).select()
-    .then(palettes => {
-      if (palettes.length) {
-        return response.status(200).json(palettes);
-      } else {
-        return response.status(404).json({
-          error: `Could not find and palettes for this project`
-        });
-      }
-    })
-    .catch( error => {
-      return response.status(500).json({error});
-    });
+  database('palettes').where('project_id', request.params.id).select().then(palettes => {
+    if (palettes.length) {
+      return response.status(200).json(palettes);
+    } else {
+      return response.status(404).json({error: `Could not find and palettes for this project`});
+    }
+  }).catch(error => {
+    return response.status(500).json({error});
+  });
 });
 
 app.post('/api/v1/projects', (request, response) => {
   const project = request.body;
 
   if (!project.project_name) {
-    return response.status(422).json({
-      error: 'This project is missing a name'
-    });
+    return response.status(422).json({error: 'This project is missing a name'});
   }
 
-  database('projects').insert(project, 'id')
-    .then(project => {
-      return response.status(201).json({ id: project[0]});
-    })
-    .catch(error => {
-      return response.status(500).json(error);
-    });
+  database('projects').insert(project, 'id').then(project => {
+    return response.status(201).json({id: project[0]});
+  }).catch(error => {
+    return response.status(500).json(error);
+  });
 });
 
-app.post('/api/v1/projects/:id/footnotes', (request, response) => {
+app.post('/api/v1/projects/:id/palettes', (request, response) => {
   let palette = request.body;
   const projectId = request.params.id;
 
-  for (let requiredParameter of ['name', 'color_1', 'color_2',
-    'color_3', 'color_4', 'color_5', 'project_id']) {
+  for (let requiredParameter of['name',
+    'color_1',
+    'color_2',
+    'color_3',
+    'color_4',
+    'color_5',
+    'project_id']) {
     if (!palette[requiredParameter]) {
-      return response.status(422).json({
-        error: `You are missing the ${requiredParameter} property`
-      });
+      return response.status(422).json({error: `You are missing the ${requiredParameter} property`});
     }
   }
 
-  palette = Object.assign({}, palette, { project_id: projectId });
+  palette = Object.assign({}, palette, {project_id: projectId});
 
-  database('palettes').insert(palette, 'id')
-    .then(palette => {
-      return response.status(201).json({ id: palette[0]});
-    })
-    .catch(error => {
-      return response.status(500).json({ error });
-    });
+  database('palettes').insert(palette, 'id').then(palette => {
+    return response.status(201).json({id: palette[0]});
+  }).catch(error => {
+    return response.status(500).json({error});
+  });
 });
 
 app.listen(app.get('port'), () => {
