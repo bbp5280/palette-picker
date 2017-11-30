814c31d1245bd250f020b15a4c0c36f8bb689e48
Ben Porter <bbp5280@gmail.com>
2017-11-30 07:05:08 -0700

    Add post route for projects

--- server.js
+++ server.js
@@ -1,6 +1,6 @@
 const express = require('express');
 const app = express();
-const bodyParser = require('body-parser')
+const bodyParser = require('body-parser');
 const path = require('path');
 
 app.set('port', process.env.PORT || 3000);
@@ -14,28 +14,53 @@ const database = require('knex')(configuration);
 
 app.get('/api/v1/projects', (request, response) => {
   database('projects').select()
-  .then(projects => {
-    return response.status(200).json(projects);
-  })
-  .catch(error => {
-    return response.status(500).json(error);
-  })
+    .then(projects => {
+      return response.status(200).json(projects);
+    })
+    .catch(error => {
+      return response.status(500).json(error);
+    });
 });
 
 app.get('/api/v1/projects/:id/palettes', (request, response) => {
   database('palettes').where('project_id', request.params.id).select()
     .then(palettes => {
-      if(palettes.length) {
+      if (palettes.length) {
         return response.status(200).json(palettes);
       } else {
         return response.status(404).json({
           error: `Could not find and palettes for this project`
-        })
+        });
       }
     })
     .catch( error => {
-      return response.status(500).json({error})
+      return response.status(500).json({error});
+    });
+});
+
+app.post('/api/v1/projects', (request, response) => {
+  const project = request.body;
+
+  if (!project.project_name) {
+    return response.status(422).json({
+      error: 'This project is missing a name'
+    });
+  }
+
+  database('projects').insert(project, 'id')
+    .then(project => {
+      return response.status(201).json({ id: project[0]});
     })
+    .catch(error => {
+      return response.status(500).json(error);
+    });
+});
+
+app.post('/api/v1/projects/:id/footnotes', (request, response) => {
+  let palette = request.body;
+  const projectId = request.params.id;
+
+  for (let requiredParameter of ['', ])
 })
 
 app.listen(app.get('port'), () => {
