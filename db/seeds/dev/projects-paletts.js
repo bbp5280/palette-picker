ad8123de00a836e22825b09dba5c116e226667ee
Ben Porter <bbp5280@gmail.com>
2017-11-29 20:57:22 -0700

    Add seed data to db

---
+++ db/seeds/dev/projects-paletts.js
@@ -0,0 +1,38 @@
+exports.seed = function(knex, Promise) {
+  // Deletes ALL existing entries
+  return knex('palettes').del()
+    .then(() => knex('projects').del())
+
+    .then(() => {
+      return Promise.all([
+        knex('projects').insert({
+          project_name: 'Hot Fire'
+        }, 'id')
+          .then(project => {
+            return knex('palettes').insert([
+              {
+                name: 'Bright',
+                color_1: '#e9476d',
+                color_2: '#64297f',
+                color_3: '#faab5e',
+                color_4: '#89d003',
+                color_5: '#c7e73a',
+                project_id: project[0]
+              },
+              {
+                name: 'Yikes',
+                color_1: '#580d88',
+                color_2: '#81d644',
+                color_3: '#70d59e',
+                color_4: '#b6f0f1',
+                color_5: '#708409',
+                project_id: project[0]
+              }
+            ]);
+          })
+          .then(() => console.log('Seeding All Good!'))
+          .catch(error => console.log({ error }))
+      ]);
+    })
+    .catch(error => console.log({ error }));
+};
